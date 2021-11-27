43bd7a3cae9d0a38ffedd1774572e5e7152bf544
aquarrion <garyaguilar300@gmail.com>
2021-11-27 14:35:39 -0400

    Busqueda binaria y secuencial

--- src/Ejercicio.java
+++ src/Ejercicio.java
@@ -27,4 +27,39 @@ public class Ejercicio {
         return indiceVector(vector, 0) > 0;
     }
 
+
+    //busqueda binaria
+    //busqueda secuencial
+
+    /** Buscar tablas desordenadas Búsqueda secuencial */
+    public static int busquedaSecuencial(int[] listaDatos, int objeto) {
+        for (int i = 0; i < listaDatos.length; i++) {
+            if (listaDatos[i] == objeto)
+                return i;
+        }
+        return -1;
+    }
+
+    /**
+     * Búsqueda binaria, para listas ordenadas
+     */
+    public static int busquedaBinaria(int[] listaDatos, int objeto) {
+        int mid = 0;
+        int first = 0;
+        int last = listaDatos.length - 1;
+
+        while (first <= last) {
+            mid = (first + last) / 2;
+            if (objeto == listaDatos[mid])
+                return mid;
+            else {
+                if (objeto > listaDatos[mid])
+                    first = mid + 1;
+                else
+                    last = mid - 1;
+            }
+        }
+        return -1;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -18,7 +18,12 @@ public class Main {
         /*int[] vector={10,8,2,6,4,2,1,0};
         System.out.println(Ejercicio.indiceVector(vector));*/
 
+        //indicevector-2
+        int datalist1[] = {23, 56, 73, 77, 26, 65, 41, 34, 12, 74, 37};
+        int datalist2[] = {23, 28, 33, 34, 41, 47, 59, 71, 77, 82, 90};
 
+        System.out.println(Ejercicio.busquedaSecuencial(datalist1,77));
+        //System.out.println(Ejercicio.busquedaBinaria(datalist2,33));ntln(Ejercicio.sumaVector(vector, 0));*/
 
     }
 }
